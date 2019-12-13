ba5fee28f8d97309a74f0938a27a693dbfd94d51
Darian Jarvis <djarvis@nyc.yearup.org>
2019-12-13 13:53:47 -0500

    updated the title styles.

--- public/main.js
+++ public/main.js
@@ -1,26 +1,40 @@
 headlineBG = document.getElementById('headline')
 headlinetitle = document.getElementById('headline--title')
 subHeadlineClick = document.getElementById('headline--click')
+subHeadlineClickLink = document.getElementById('headline--clicklink')
+
 function toggleHeadlineImg(){
   headlineBG.addEventListener('click',() => {
     console.log('it works');
   })
 }
-
 toggleHeadlineImg()
 
 
-setTimeout(()=> {
-  headlinetitle.style.animation = "fade-in 1s ease-in";
-  subHeadlineClick.style.display = 'none';
-// }
-},1000)
-setTimeout(()=> {
-  subHeadlineClick.style.display = 'block';
 
-  subHeadlineClick.style.animation = "fade-in 1s ease-in";
-// }
-},1500)
+var linkIsClicked = false;
+console.log(linkisClicked);
+
+function clickHandler() {
+  linkIsClicked = true;
+
+}
+
+if (linkIsClicked != true){
+  setTimeout(()=> {
+    headlinetitle.style.animation = "fade-in 1s ease-in";
+    subHeadlineClick.style.display = 'none';
+  // }
+  },1000)
+  setTimeout(()=> {
+    subHeadlineClick.style.display = 'block';
+
+    subHeadlineClick.style.animation = "fade-in 1s ease-in";
+    linkIsClicked = true;
+  // }
+  },1500)
+
+}
 
 
 
